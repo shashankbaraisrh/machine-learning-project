eca9ddf5da8c76a155bfe2f5d2c7e2b2928e9c2d
shashankbaraisrh <147604615+shashankbaraisrh@users.noreply.github.com>
2024-07-26 23:19:26 +0200

    Hush-Hush-Recruiter

--- hush_hush_recruiter/code/Hush-Hush _Recruiter/github_candidates_selection/1.1githubapi_unseen_json.py
+++ hush_hush_recruiter/code/Hush-Hush _Recruiter/github_candidates_selection/1.1githubapi_unseen_json.py
@@ -6,7 +6,7 @@ class GitHubDataFetcher:
     def __init__(self):
         self.headers = {
             "Accept": "application/vnd.github+json ",
-            "Authorization": "Bearer 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",
+            #"Authorization": "",
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.59',
             "X-GitHub-Api-Version": "2022-11-28"
         }
